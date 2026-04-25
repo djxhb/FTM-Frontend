9ab37d6a1209c157cc03bdb87eebbf13a1fd530d
djxhb <93751361+djxhb@users.noreply.github.com>
2026-04-26 02:57:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/calculate`;
-
+//const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/calculate`;
+const API_URL = "https://api.medlight.tech/calculate";
 const fields = [
   { key: "Age", options: [80,75,70,65,60,55,50,45,40,35,30,25], defaultIndex: 6 },
   { key: "BMI", options: [18,22,26,30,34], defaultIndex: 2 },
